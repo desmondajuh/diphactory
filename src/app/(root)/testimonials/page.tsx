135c960ce164cf96c4d17a14b573ad9a1ca7107e
desmondajuh <desmondajuh@gmail.com>
2026-04-17 01:08:32 +0100

    landing pages setup

--- src/app/(root)/testimonials/page.tsx
+++ src/app/(root)/testimonials/page.tsx
@@ -1,1 +1,9 @@
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
+
+export default function TestimonialsPage() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">Testimonials Page</h1>
+    </div>
+  );
+}
