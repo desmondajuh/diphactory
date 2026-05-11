d6eb1dce3696f8df239c6977ceb0c8118a23ed2a
desmondajuh <desmondajuh@gmail.com>
2026-05-11 01:32:44 +0100

    og image edge function removal

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -2,7 +2,7 @@
 
 import { ImageResponse } from "next/og";
 
-export const runtime = "edge";
+// export const runtime = "edge";
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
